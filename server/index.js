6560b3cafec6283560ad92fd4578fc81dfd24724
Aryan Jain <adityajain@adityas-Laptop.local>
2024-07-26 20:34:42 +0530

    set up the cors

--- server/index.js
+++ server/index.js
@@ -17,18 +17,8 @@ app.use(express.static(path.resolve("./public")));
 dbConnection(process.env.DB_URL);
 
 // Middleware
-const allowedOrigins = ['https://car-dealership-frontend-eta.vercel.app/'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+// const allowedOrigins = ['https://car-dealership-frontend-eta.vercel.app/'];
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
