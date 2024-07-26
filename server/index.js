bad51e0a51526ed92d64b4899c9bd8d839182a49
Aryan Jain <adityajain@adityas-Laptop.local>
2024-07-26 20:43:05 +0530

    add the url and cors origin

--- server/index.js
+++ server/index.js
@@ -1,37 +1,45 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors"); // Import cors middleware
+const cors = require("cors");
 const dbConnection = require("./database/db");
 const PORT = process.env.PORT || 3000;
 const userRoutes = require("./routes/userRoutes.js");
 const cookieParser = require("cookie-parser");
-const path = require("path")
-const carRoutes = require("./routes/carRoutes.js")
-const cartRoutes = require("./routes/cartRoutes.js")
-
-
+const path = require("path");
+const carRoutes = require("./routes/carRoutes.js");
+const cartRoutes = require("./routes/cartRoutes.js");
 
 const app = express();
+
+// CORS configuration
+const corsOptions = {
+  origin: 'https://car-dealership-frontend-eta.vercel.app', // Allow only this origin
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow these methods
+  allowedHeaders: ["Content-Type", "Authorization"] // Allow these headers
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Handle preflight requests
+
+
+// Serve static files
 app.use(express.static(path.resolve("./public")));
 
+// Connect to the database
 dbConnection(process.env.DB_URL);
 
 // Middleware
-// const allowedOrigins = ['https://car-dealership-frontend-eta.vercel.app/'];
-app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
-app.get("/", (req,res) => {
-    res.send("Hello world server is running 🤣")
-})
+app.get("/", (req, res) => {
+  res.send("Hello world server is running 🤣");
+});
 app.use("/user", userRoutes);
 app.use("/car", carRoutes);
 app.use("/cart", cartRoutes);
 
-
-
+// Start the server
 app.listen(PORT, () => console.log("Server is running on port", PORT));
