7b9d32a05f2511509af519331dafda99ff9490c6
Aryan Jain <adityajain@adityas-Laptop.local>
2024-07-29 07:32:49 +0530

    add the dotenv

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async (url) => {
   try {
-    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(url);
     console.log("MongoDB Successfully Connected");
   } catch (err) {
     console.error("Error while connecting to MongoDB:", err);
