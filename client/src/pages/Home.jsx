d42e0ceb93d0ba53ae2ab6c84bf5e1913bef071b
Aryan Jain <adityajain@adityas-Laptop.local>
2024-07-29 20:09:59 +0530

    clean up the homne page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,12 +14,12 @@ const Home = () => {
   return (
     <div className='bg-black w-screen h-screen'>
       <Navbar />
-      <h1 className="text-white text-center mt-8 text-3xl">Welcome to Car Dealership</h1>
-      
-      <img src={bg} className='w-full h-[700px] object-cover' alt="" />
-      <button onClick={handleClickstart} className='mx-auto px-4 py-2 text-white bg-gray-900 rounded-md block hover:bg-gray-800'>
+      <h1 className="text-white text-center mt-8 mb-4 xl:mb-6 text-3xl">Welcome to Car Dealership</h1>
+      <button onClick={handleClickstart} className='mx-auto mt-6 px-4 py-2 text-white bg-gray-900 rounded-md block hover:bg-gray-800'>
         Get Started
       </button>
+      <img src={bg} className='w-full h-[700px] object-cover' alt="" />
+      
 
     </div>
   );
